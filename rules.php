b8176ce85f9b33022f6b06e180434053be1b3d87
TwinDad <mark@chfrrailroad.net>
2015-12-28 12:48:36 -0500

    Adding initial files

---
+++ rules.php
@@ -0,0 +1,75 @@
+<?php
+//Put page title here
+  $page_title="CH&amp;FR General Rules";
+// Put page header subtitle here
+  $page_subtitle="Employee Website";
+
+ include("include/header.html"); 
+ include("include/body_header.html"); 
+?>
+
+
+<div id="content">
+
+  <div id="page_subhead">
+    General Operating and Safety Rules<br>For All Subdivisions
+  </div>
+<br>
+The following rules shall be adhered to at all times on all CH&amp;FR
+Property.&nbsp; <br>
+
+<br>
+
+Employees and guests should also consult the relevant Employee
+Timetable for specific rules governing the Subdivision they will be
+visiting or operating on.&nbsp; In no case shall a rule spelled out in
+an Employee Timetable supersede these General Rules.<br>
+
+<ol>
+
+  <li>The safety of crew, passengers, workers and civilians is
+paramount.&nbsp; No operations are permitted that would compromise the
+safety of anyone.&nbsp; This rule supersedes all other rules.&nbsp;
+This rule is referred to as "GOR#1" elsewhere in this document.</li>
+  <li>No operations which would damage equipment is permitted, except
+where avoiding equipment damage would result in a safety hazard to
+personnel or bystanders.</li>
+  <li>No unauthorized personnel are permitted to operate the
+equipment.&nbsp; Untrained guests must be directly supervised by the
+Train Master or a trained employee designated by the Train Master.</li>
+  <li>Chain of Command shall be strictly followed.&nbsp; The Train
+Master is the final authority on all operations and activities on the
+Railroad.&nbsp; The Conductor is in charge of the operation of the
+train, and the Engineer is in charge of the Locomotive Consist.&nbsp;
+Disputes between the Conductor and Engineer shall be resolved in
+accordance with these rules, or arbitrated by the Train Master.</li>
+  <li>No Train may exceed the posted maximum speeds without the express
+permission and supervision of the Train Master.&nbsp; No Train Order or
+Track Warrant may authorize exceeding the posted speed limits unless
+personally signed by the Train Master.</li>
+  <li>Switches and other operator controls shall be operated in
+accordance with the specific rules set out in the relevant subsections
+of the Employee Timetable.</li>
+  <li>No Train may be intentionally crashed or derailed, except as
+necessary to conform to GOR#1 above.</li>
+  <li>
+    <div><span class="modelnote">Rolling stock must be kept on the
+track or in storage at all times.</span></div>
+  </li>
+  <li>
+    <div><span class="modelnote">No handling of rolling stock except
+for coupling/uncoupling or re-railing.</span></div>
+  </li>
+  <li>No alcoholic beverages or other drugs are to be used during
+railroad operations, nor for 12 hours prior to commencing operations
+("12 hours bottle to throttle").&nbsp; Employees or guests suspected of
+abusing drugs or alcohol on or off duty shall be suspended indefinitely
+and urged to seek counseling.</li>
+  <li>No food or drink in the railroad operating area, except at
+designated locations and with the permission of the Train Master.<br>
+  </li>
+  <li>Despite all these seemingly onerous rules, employees and guests are encouraged to HAVE FUN!</li>
+</ol>
+</div>
+
+</body></html>
---
+++ timetable_full.php
@@ -0,0 +1,88 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html> <head>
+<title>Operations: CH&FR Railroad</title>
+<link rel="shortcut icon" href="CHFRLogo80.ico">
+<link rel="stylesheet" href="common.css" type="text/css">
+<link rel="stylesheet" href="page.css" type="text/css">
+<link rel="stylesheet" href="headerfooter.css" type="text/css">
+<link rel="stylesheet" href="timetable.css" type="text/css">
+</head>
+
+<body>
+<div class="page">
+<div class="header_short">
+  <div class="img_container">
+  <img class="header_img_short" src="images/steamer960x250.jpg" alt="Header
+    Image"/>
+  <img class="header_logo" src="images/toplogo100.png" alt="logo" />
+    </div>
+</div>
+
+<div class="menu_base">
+  <div class="navbar_base">
+    <div class="navbar">
+      <ul>
+        <li id="left"><a href="index.html"><span class="clickable_div"></span></a>Home</li>
+	<li><a href="operations.html"><span class="clickable_div"></span></a>Up</li>
+        <li><a href="about.html"><span class="clickable_div"></span></a>About CH&FR</li>
+	<li><a href="aboutme.html"><span class="clickable_div"></span></a>About Me</li>
+        <li id="right"><a href="http://blog.chfrrailroad.net" target="_blank"><span class="clickable_div"></span></a>Blog</li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<div class="content_container">
+  <div class="content_box">
+<div class="content">
+<a name="top"</a><br>
+
+  <div class="page_subhead">
+    CH&FR Railroad Employee Timetable
+  </div>
+
+<p>This document provides the General and Special rules in force on the
+Chestnut Hill &amp; Frost River Railroad.&nbsp;
+All employees of the CH&FR shall be familiar with the contents of
+these rules and timetables, and the conditions set forth shall be
+followed at all times.&nbsp; Exceptions to these rules may only be
+permitted by explicit order of the Train Master.&nbsp;</p> <p>
+Timetable information may be overridden by specific Train Order or
+Track warrant.</p>
+
+<div class="modelnote">
+<p>Note:
+Because this document is both a (fictional) &#8220;prototype&#8221; Rulebook and a
+guide for operations of a model railroad, special model operations
+notes are indicated in some subsections to indicate the equivalent
+model action to the prototype rule.&nbsp; These special notes are noted
+in red italics &#8211; just like this one.</p></div>
+
+<h2><a name="TOC"></a>Table of Contents</h2>
+  <ol id="TOC">
+  <li><a href="#s1_rules">General Operating Rules</a></li>
+  <li><a href="#s2_instructions">Special Instructions</a></li>
+  <li><a href="#s3_movement">Train Movements</a></li>
+  <li><a href="#s4_frostriver">Frost River Sub</a></li>
+  <li><a href="#s5_gloversbend">Glover's Bend Sub</a></li>
+  </ol>
+</div> <!-- content -->
+</div> <!-- content_box -->
+
+<?php include("ttable_common.html"); ?>
+<?php include("ttable_frostriver.html"); ?>
+<?php include("ttable_gbend.html"); ?>
+
+<div class="content_box">
+<div class="content">
+<a href="#top">[TOP]</a>
+
+  </div> <!-- content -->
+  </div> <!-- content_box -->
+</div> <!-- content_container -->
+
+  <div class="footer">
+    <p>(c) 2015 Mark Underwood All Rights Reserved
+  </div> <!-- footer ->
+</div> <!-- page -->
+</body> </html>
